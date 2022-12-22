3b5c0dfb425c78a3c3d9a57b26bb8ad8fdd5daa2
oludare89 <azizoyekan@hotmail.com>
2022-12-22 10:53:10 +0000

    Created InputHandler class and this.lastKey variable to detect last direction key pressed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import Player from './player.js';
+import InputHandler from './input.js';
 
 window.addEventListener('load', function(){
     const loading = document.getElementById('loading');
@@ -10,4 +11,13 @@ window.addEventListener('load', function(){
 
     const player = new Player(canvas.width, canvas.height);
     player.draw(ctx);
+    const input = new InputHandler();
+
+    function animate(){
+        console.log(input.lastKey);
+        requestAnimationFrame(animate);
+    };
+    
+    animate();
+    
 });
