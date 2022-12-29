3385564487e982f41bdf9b041efb6f8231a51075
oludare89 <azizoyekan@hotmail.com>
2022-12-29 12:00:01 +0000

    Added feature when down key released then player with stand back up

--- js/state.js
+++ js/state.js
@@ -50,6 +50,7 @@ export class SittingLeft extends State {
     handleInput(input){
         if (input === 'PRESS right') this.player.setState(states.SITTING_RIGHT);
         else if (input === 'PRESS up') this.player.setState(states.STANDING_LEFT);
+        else if (input === 'RELEASE down') this.player.setState(states.STANDING_LEFT);
     }
 }
 
@@ -64,5 +65,6 @@ export class SittingRight extends State {
     handleInput(input){
         if (input === 'PRESS left') this.player.setState(states.SITTING_LEFT);
         else if (input === 'PRESS up') this.player.setState(states.STANDING_RIGHT);
+        else if (input === 'RELEASE down') this.player.setState(states.STANDING_RIGHT);
     }
 }
